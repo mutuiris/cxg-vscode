c8a7f5906cf0863509ba6b9eb26e1bb16ee8c754
mutuiris <muneneaddy@gmail.com>
2025-07-08 18:49:02 +0300

    feat(Index): Add centralized export for all interface definitions

---
+++ packages/vscode-extension/src/analysis/interfaces/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Analysis Interfaces Barrel Export
+ * Centralized export for all interface definitions
+ */
+
+// Tree-sitter interfaces
+export * from './TreeSitterInterfaces';
+
+// Analysis result interfaces  
+export * from './AnalysisInterfaces';
